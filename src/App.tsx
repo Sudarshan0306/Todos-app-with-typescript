05b58bfbc7c658c88fb1fb3ad42a2c659be8b3d8
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-12-03 22:19:53 +0530

    created and added data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import Todos from './components/Todos';
-
+import Todos from "./components/Todos";
+import todo from "./models/todo";
 function App() {
+  const todos = [new todo("Learn React"), new todo("Learn TypeScript")];
   return (
     <div className="App">
-      <Todos />
+      <Todos items={todos} />
     </div>
   );
 }
